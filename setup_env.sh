871d79d2de746e35cfc90795432c844fd738177f
Otacon Stark <bqeye17@gmail.com>
2024-06-16 02:59:10 -0400

    Started over to clear out a lot of very bad logical issues I created trying to do stuff I didn't need to do

--- setup_env.sh
+++ setup_env.sh
@@ -1,69 +1,37 @@
 #!/bin/bash
 
-echo "Welcome to the setup environment script. The purpose of this script is to configure your environment for CTF challenges."
-echo "========================================================================================================================="
+printf "Welcome to the setup environment script. The purpose of this script is to configure your environment for CTF challenges.\n"
+printf "=========================================================================================================================\n"
 
-read -p "What is the IP address of the target machine? " ip_address
+read -p "What is the IP address of the target machine?: " ip_address
 
-echo "Adding $ip_address to ~/.zshrv or ~/.bashrc depending on your shell. Variable will be "\$ip""
+printf "Adding $ip_address to ~/.zshrc or ~/.bashrc depending on your shell. Variable will be "\$ip"\n"
 
-if [ -f ~/.zshrc ]; then
-    if grep -q "export ip"; then
-        sed 's/export ip=.*/export ip=$ip_address/' ~/.zshrc
-        source ~/.zshrc
-        echo "Updated $ip_address in ~/.zshrc"
-    else
-        echo "export ip=$ip_address" >> ~/.zshrc
-        source ~/.zshrc
-        echo "Added $ip_address to ~/.zshrc"
-    fi
-elif [ -f ~/.bashrc ]; then
-    if grep -q "export ip"; then
-        sed 's/export ip=.*/export ip=$ip_address/' ~/.bashrc
-        source ~/.bashrc
-        echo "Updated $ip_address in ~/.bashrc"
-    else
-        echo "export ip=$ip_address" >> ~/.bashrc
-        source ~/.bashrc
-        echo "Added $ip_address to ~/.bashrc"
-    fi
-else
-    echo "No shell configuration file found. Please add the following line to your shell configuration file: export ip=$ip_address"
-fi
+findShell=$(env | grep -w "SHELL" | cut -d "=" -f 2)
+ipString="export ip="
 
-echo "Setting up the directory structure for the CTF challenges"
+while true; do
 
-read -p "Where do you want the CTF directory to be stored? Format ex. ~/Documents/target_name " ctf_dir
-
-dirTest=$(test -e $ctf_dir)
-
-if [dirTest == 0]; then
-    echo "Directory already exists. Continuing..."
-    continue
-elif [dirTest == 1]; then
-    echo "Directory does not exist. Would you like to create it? (y/n)" response
-    if [response == "y"]; then
-        mkdir -p $ctf_dir && cd $ctf_dir && mkdir scans exploits notes
-        echo "Directory structure created. You can find the CTF directory at $ctf_dir"
-    elif [response == "n"]; then
-        echo "No directory created. Exiting..."
-    else
-        echo "Invalid response. Please enter y or n"
-    fi
-
-read -p "Do you have any hostnames you would like to add? (y/n) " response
-
-if [response == "y"]; then
-    read -p "What is the hostname? " hostname
-        if [grep -q "$ip_address" == 0]; then
-            sed 's/$ip_address.*/$ip_address $hostname/' /etc/hosts
-            continue
-        else 
-            echo "$ip_address $hostname" >> /etc/hosts
-            echo "Added $hostname to /etc/hosts"
+    if [ $findShell = "/usr/bin/zsh" ]; then
+        if grep -q "$ip_string" ~/.zshrc; then
+            sed -i '/export ip=*/c\export ip=\"$ip_address\"\' ~/.zshrc
+            printf "Upated the ip address variable for your shell to \$ip\n"
+        else
+            echo "export ip= $ip_address" >> ~/.zshrc
+            printf "Added $ip_address to ~/.zshrc\n"
         fi
-elif [response == "n"]; then
-    echo "No hostnames to add"
-else
-    echo "Invalid response. Please enter y or n"
-fi
+    fi
+
+    if [ $findShell = "/usr/bin/bash" ]; then
+        if grep -q "$ip_string" ~/.bashrc; then
+            sed -i '/export ip=*/c\export ip=\"$ip_address\"\' ~/.bashrc
+            printf "Updated the ip address variable for your shell to \$ip\n"
+        else
+            echo "export ip= $ip_address" >> ~/.bashrc
+            printf "Added $ip_address to ~/.bashrc\n"
+        fi
+    fi
+
+    if [ $findShell = "" ]; then
+        printf "No shell configuration file found. Please add the following line to your shell configuration file using the following command: echo export ip=\"$ip_address\" >> PATH_TO_YOUR_SHELL"
+    fi
